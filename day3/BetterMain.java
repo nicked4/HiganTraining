224a53ff25b498089c3d9b89d5c3fed8c376dd59
ikeda <daikenkai3542@gmail.com>
2021-11-21 10:40:19 +0900

    Fix indent

--- day3/BetterMain.java
+++ day3/BetterMain.java
@@ -8,34 +8,34 @@ import model.Person;
 class BetterMain {
     static String peopleToString(List<Person> people) {
         return String.join("\n", people.stream()
-                                       .map(p -> p.toString())
-                                       .collect(Collectors.toList()));
+                .map(p -> p.toString())
+                .collect(Collectors.toList()));
     }
 
     public static void main(String[] args) {
         int n = 15;
         List<Person> people = Stream.generate(Person::randomGenerate)
-                                    .limit(n)
-                                    .collect(Collectors.toList());
+                .limit(n)
+                .collect(Collectors.toList());
         
         System.out.println("*** People sorted by height ***");
         List<Person> sortedPeople = people.stream()
-                                          .sorted(Comparator.comparing(Person::getHeight).reversed())
-                                          .collect(Collectors.toList());
+                .sorted(Comparator.comparing(Person::getHeight).reversed())
+                .collect(Collectors.toList());
         System.out.println(peopleToString(sortedPeople));
 
         System.out.println("\n*** People under 60kg ***");
         List<Person> under60kgPeople = people.stream()
-                                             .filter(p -> p.getWeight() < 60)
-                                             .collect(Collectors.toList());
+                .filter(p -> p.getWeight() < 60)
+                .collect(Collectors.toList());
         System.out.println(peopleToString(under60kgPeople));
 
         System.out.println("\n*** People BMI ***");
         List<Double> bmis = people.stream()
-                                  .map(p -> p.calcBmi())
-                                  .collect(Collectors.toList());
+                .map(p -> p.calcBmi())
+                .collect(Collectors.toList());
         System.out.println(bmis.stream()
-                               .map(b -> String.format("%.2f", b))
-                               .collect(Collectors.toList()));
+                .map(b -> String.format("%.2f", b))
+                .collect(Collectors.toList()));
     }
 }
